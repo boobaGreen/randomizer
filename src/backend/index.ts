22e5800790b462d4540bbd30d7a4562a65dd8ce8
boobaGreen <devboobagreen@gmail.com>
2024-03-28 02:49:16 +0100

    back

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,26 +1,48 @@
-import { Server } from 'azle';
-import express, { Request } from 'express';
+import { jsonStringify, Server } from "azle";
+import express, { Request } from "express";
 
 let db = {
-    hello: ''
+  hello: "",
 };
 
 export default Server(() => {
-    const app = express();
+  const app = express();
 
-    app.use(express.json());
+  console.log("back start 3");
+  app.use(express.json());
 
-    app.get('/db', (req, res) => {
-        res.json(db);
-    });
+  app.get("/db", (req, res) => {
+    res.json(db);
+  });
 
-    app.post('/db/update', (req: Request<any, any, typeof db>, res) => {
-        db = req.body;
+  app.post("/db/update", (req: Request<any, any, typeof db>, res) => {
+    db = req.body;
 
-        res.json(db);
-    });
+    res.json(db);
+  });
 
-    app.use(express.static('/dist'));
+  app.post("/randomness", async (req, res) => {
+    const response = await fetch("icp://aaaaa-aa/raw_rand");
+    const responseJson = await response.json();
+    console.log("response json", responseJson);
 
-    return app.listen();
+    const stringify = jsonStringify(responseJson);
+    // Parsa la stringa JSON
+    const parsedObject = JSON.parse(stringify);
+
+    // Estrai l'array di uint8
+    const uint8array = parsedObject["__uint8array__"];
+
+    // Estrapola la seconda parte dell'array
+    const secondPart = uint8array.slice(1);
+
+    // Estrapola il primo valore
+    const primoValore = uint8array[0];
+
+    res.send([primoValore]);
+  });
+
+  app.use(express.static("/dist"));
+
+  return app.listen();
 });
