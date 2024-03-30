cb4fa80d25b6205b7a34d3cfc90bed2f22c5a900
boobaGreen <devboobagreen@gmail.com>
2024-03-30 02:57:44 +0100

    funziona v 0.2 - andare avanti con gestion errori base dell api

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -13,55 +13,41 @@ let query = {
 export default Server(() => {
   const app = express();
 
-  console.log("back start 3");
+  console.log("back start ");
   app.use(express.json());
 
-  app.get("/db", (req, res) => {
-    res.json(db);
-  });
-
-  app.post("/db/update", (req: Request<any, any, typeof db>, res) => {
-    console.log("req.body", req.body);
-    db = req.body;
-    // console.log("Request", Request);
-    res.json(db);
-  });
-
   app.post("/randomness", async (req: Request<any, any, typeof query>, res) => {
-    // console.log("req", req);
-    console.log("req.body", req.body);
-    // console.log("req : ", req);
-    const response = await fetch("icp://aaaaa-aa/raw_rand");
-    const responseJson = await response.json();
-    // console.log("response json", responseJson);
-    const stringify = jsonStringify(responseJson);
-    // Parsa la stringa JSON
-    const parsedObject = JSON.parse(stringify);
-
-    // Estrai l'array di uint8
-    const uint8array = parsedObject["__uint8array__"];
-
-    // Estrapola la seconda parte dell'array
-    const secondPart = uint8array.slice(1);
-    console.log("second part", secondPart);
-    // Estrapola il primo valore
-    // const primoValore = uint8array[0];
-
-    let finalArray = [];
-
-    for (let i = 0; i < req.body.draws; i++) {
-      finalArray[i] = Math.round(
-        (secondPart[i] * (req.body.participants - 1)) / 255 + 1
+    try {
+      console.log("req.body", req.body);
+      if (req.body.participants < 1 || req.body.participants > 256) {
+        throw new Error("Partcipants number must be 1-256");
+      }
+      const response = await fetch("icp://aaaaa-aa/raw_rand");
+      const responseJson = await response.json();
+      const stringify = jsonStringify(responseJson);
+      const parsedObject = JSON.parse(stringify);
+      const uint8array = parsedObject["__uint8array__"];
+      const secondPart = uint8array.slice(1);
+      let finalArray = [];
+      for (let i = 0; i < req.body.draws; i++) {
+        finalArray[i] = Math.round(
+          (secondPart[i] * (req.body.participants - 1)) / 255 + 1
+        );
+      }
+      console.log("finale array", finalArray);
+      res.send(finalArray);
+    } catch (error) {
+      console.error(
+        "Si è verificato un errore durante la generazione dei numeri casuali:",
+        error
       );
+      res
+        .status(500)
+        .send(
+          "Si è verificato un errore durante la generazione dei numeri casuali"
+        );
     }
-
-    // const randomNumberInRange = Math.round(
-    //   (primoValore * (req.body.participants - 1)) / 255 + 1
-    // );
-    console.log("finale array", finalArray);
-    res.send(finalArray);
   });
-
   app.use(express.static("/dist"));
 
   return app.listen();
